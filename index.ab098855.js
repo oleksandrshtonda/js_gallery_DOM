3526d1c40e30a7f2c2d472b2eb7a70c1350f2738
oleksandrshtonda <oleksandrshtonda@gmail.com>
2024-02-17 12:22:51 +0100

    make build

---
+++ index.ab098855.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(r){r.preventDefault();var t=r.target.closest("a");t&&(e.src=t.href)});//# sourceMappingURL=index.ab098855.js.map
+
+//# sourceMappingURL=index.ab098855.js.map
